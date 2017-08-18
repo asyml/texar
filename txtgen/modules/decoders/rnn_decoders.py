e372227076c652c071ed572557f3bd118080983d
Zhiting Hu <zhitinghu@gmail.com>
2017-08-18 14:45:17 -0400

    finished basic rnn decoder

--- txtgen/modules/decoders/rnn_decoders.py
+++ txtgen/modules/decoders/rnn_decoders.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tensorflow as tf
 from tensorflow.contrib.seq2seq import BasicDecoderOutput
 from tensorflow.python.framework import tensor_shape, dtypes    # pylint: disable=E0611
 
@@ -14,7 +15,7 @@ from txtgen.modules.decoders.rnn_decoder_base import RNNDecoderBase
 
 
 class BasicRNNDecoder(RNNDecoderBase):
-    """Basic RNN decoder that performs simple sampling at each step.
+    """Basic RNN decoder that performs sampling at each step.
     """
 
     def __init__(self, vocab_size, cell=None, hparams=None,
@@ -23,13 +24,24 @@ class BasicRNNDecoder(RNNDecoderBase):
         self._vocab_size = vocab_size
 
     def initialize(self, name=None):
-        pass
+        return self._helper.initialize() + (self._initial_state,)
 
     def step(self, time, inputs, state, name=None):
-        pass
+        cell_outputs, cell_state = self._cell(inputs, state)
+        logits = tf.contrib.layers.fully_connected(
+            inputs=cell_outputs, num_outputs=self._vocab_size)
+        sample_ids = self._helper.sample(
+            time=time, outputs=logits, state=cell_state)
+        (finished, next_inputs, next_state) = self._helper.next_inputs(
+            time=time,
+            outputs=logits,
+            state=cell_state,
+            sample_ids=sample_ids)
+        outputs = BasicDecoderOutput(logits, sample_ids)
+        return (outputs, next_state, next_inputs, finished)
 
     def finalize(self, outputs, final_state, sequence_lengths):
-        pass
+        return outputs, final_state
 
     @property
     def output_size(self):
