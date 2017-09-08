f7099a1d9db8da53304da8716d401b989f000b46
snakeztc <zhaotiancheng.hz@gmail.com>
2017-09-08 15:14:42 -0400

    add example code highlight

--- txtgen/core/layers.py
+++ txtgen/core/layers.py
@@ -15,42 +15,44 @@ from txtgen.core.utils import get_instance, switch_dropout
 
 def default_rnn_cell_hparams():
     """Returns default hyperparameters of an RNN cell.
-
+    
     Returns:
         A dictionary of default hyperparameters of an RNN cell, with the
-        following structure and values::
-
+        following structure and values:
+        
+        .. code-block:: python
+        
             {
                 "cell": {
                     # Name or full path of the cell class. E.g., the classname
                     # of built-in cells in `tensorflow.contrib.rnn`, or the
                     # classname of user-defined cells in `txtgen.custom`, or a
                     # full path like "my_module.MyCell".
-
+    
                     "type": "BasicLSTMCell",
-
+    
                     # A dictionary of arguments for constructor of the cell
                     # class. An RNN cell is created by calling the cell class
                     # named in `type` passing the arguments specified in
                     # `kwargs` as `cell_class(**kwargs)`
-
+    
                     "kwargs": {
                         "num_units": 64
                     }
                 },
-
+    
                 "num_layers": 1       # Number of cell layers
-
+    
                 # Dropout applied to the cell in each layer. See
                 # `tensorflow.contrib.rnn.DropoutWrapper` for each of the
                 # hyperparameters. If all keep probablities are 1.0, no dropout
                 # is applied.
-
+    
                 "dropout": {
                     "input_keep_prob": 1.0,
                     "output_keep_prob": 1.0,
                     "state_keep_prob": 1.0,
-
+    
                     # If True, the same dropout mask is applied at every step,
                     # and the list of input size of each layer is required
                     # (in "input_size"). The input size of a layer is the size
@@ -58,22 +60,21 @@ def default_rnn_cell_hparams():
                     # input size of the first layer is usually the dimension of
                     # word embeddings, while the input size of followup layers
                     # are usually the num_units of the cells.
-
+    
                     "variational_recurrent": False,
                     "input_size": []
                 },
-
+    
                 # If True, apply residual connection on the inputs and
                 # outputs of cell in each layer except the first layer.
-
+    
                 "residual": False,
-
+    
                 # If True, apply highway connection on the inputs and
                 # outputs of cell in each layer except the first layer.
-
+    
                 "highway": False,
             }
-
     """
     return {
         "cell": {
@@ -96,8 +97,10 @@ def default_rnn_cell_hparams():
 
 
 def get_rnn_cell(hparams):
-    """Creates an RNN cell.
-
+    """
+    
+    Creates an RNN cell.
+       
     Args:
         hparams (dict or HParams): Cell hyperparameters.
 
