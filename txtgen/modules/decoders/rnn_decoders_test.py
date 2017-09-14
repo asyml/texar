8cbbdc3bdc2ed2e9252e6678af9be27b94dc2b82
Zhiting Hu <zhitinghu@gmail.com>
2017-09-14 17:29:01 -0400

    starting rnn decoder unit tests

---
+++ txtgen/modules/decoders/rnn_decoders_test.py
@@ -0,0 +1,23 @@
+#
+"""
+Unit tests for RNN decoders.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import tensorflow as tf
+
+from txtgen.modules.decoders.rnn_decoders import BasicRNNDecoder
+
+
+class BasicRNNDecoderTest(tf.test.TestCase):
+    """Test basic RNN decoder.
+    """
+    pass
+
+if __name__ == "__main__":
+    tf.test.main()
+
