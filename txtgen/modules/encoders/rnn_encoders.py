c447b076cadeb7842613da37e0f7f93216ccbe3d
snakeztc <zhaotiancheng.hz@gmail.com>
2017-09-28 13:12:35 -0400

    add hierarchical encoder

--- txtgen/modules/encoders/rnn_encoders.py
+++ txtgen/modules/encoders/rnn_encoders.py
@@ -8,6 +8,8 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+from tensorflow.python.ops import array_ops
+from tensorflow.contrib.rnn import LSTMStateTuple
 
 from txtgen.modules.encoders.encoder_base import EncoderBase
 from txtgen.core import layers
@@ -103,7 +105,9 @@ class RNNEncoderBase(EncoderBase):
         """
         return {
             "rnn_cell": layers.default_rnn_cell_hparams(),
-            "embedding": layers.default_embedding_hparams()
+            "embedding": layers.default_embedding_hparams(),
+            "minor_type": "rnn",
+            "minor_cell": layers.default_rnn_cell_hparams()
         }
 
     def _build(self, inputs, *args, **kwargs):
@@ -185,6 +189,106 @@ class ForwardRNNEncoder(RNNEncoderBase):
                 cell=self._cell,
                 inputs=embedded_inputs,
                 **kwargs)
+
+        self._add_internal_trainable_variables()
+        # Add trainable variables of `self._cell` which may be constructed
+        # externally
+        self._add_trainable_variable(self._cell.trainable_variables)
+        self._built = True
+
+        return results
+
+
+class HierarchicalForwardRNNEncoder(RNNEncoderBase):
+    """One directional forward RNN encoder with 2 levels. Useful for encoding
+       structured long sequences, e.g. paragraphs, dialogs and etc.
+       Expect 3D tensor input [B, T, U] 
+       B: batch size T: the major seq len U:the minor seq len
+       The minor encoder encodes the inputs along the 2 axis
+       The major encoder encodes the inputs along the 1st axis
+       
+       the minor encoder supports various types: RNN, bi-RNN, CNN, CBOW etc.
+
+      See :class:`~txtgen.modules.encoders.rnn_encoders.RNNEncoderBase` for the
+      arguments, and
+      :class:`~txtgen.modules.encoders.rnn_encoders.RNNEncoderBase.`
+      `default_hparams` for the default hyperparameters.
+      """
+
+    def __init__(self,  # pylint: disable=too-many-arguments
+                 cell=None,
+                 embedding=None,
+                 embedding_trainable=True,
+                 vocab_size=None,
+                 hparams=None,
+                 name="hierarhical_forward_rnn_encoder"):
+        RNNEncoderBase.__init__(
+            self, cell, embedding, embedding_trainable,
+            vocab_size, hparams, name)
+
+        if self._hparams.minor_type == 'rnn':
+            # Make minor rnn cell
+            with tf.variable_scope(self.variable_scope):  # pylint: disable=not-context-manager
+                self._minor_cell = layers.get_rnn_cell(self._hparams.minor_cell)
+
+        elif self._hparams.minor_type == 'bow':
+            raise ValueError("BOW encoder is not yet supported")
+
+        elif self._hparams.minor_type == 'cnn':
+            raise ValueError("CNN encoder is not yet supported")
+
+        else:
+            raise ValueError("Unknown minor type {}"
+                             .format(self._hparams.minor_type))
+
+    def _build(self, inputs, **kwargs):
+        """Encodes the inputs.
+
+        Args:
+            inputs:  3D tensor input [B, T, U] 
+            **kwargs: Optional keyword arguments of `tensorflow.nn.dynamic_rnn`,
+                such as `sequence_length`, `initial_state`, etc.
+
+        Returns:
+            Outputs and final state of the encoder.
+        """
+
+        major_len = array_ops.shape(inputs)[1]
+        minor_len = array_ops.shape(inputs)[2]
+        embed_dim = self.hparams.embedding.dim
+        minor_cell_size = self.hparams.minor_cell.cell.kwargs.num_units
+
+        embedded_inputs = tf.nn.embedding_lookup(self._embedding, inputs)
+
+        # B x T x U x E - > (BxT) x U x E
+        flat_embedded_inputs = tf.reshape(embedded_inputs,
+                                          [-1, minor_len, embed_dim])
+
+        with tf.variable_scope("minor_rnn"):
+            minor_outs, minor_states = tf.nn.dynamic_rnn(
+                cell=self._minor_cell,
+                inputs=flat_embedded_inputs,
+                dtype=tf.float32)
+
+            if type(minor_states) is LSTMStateTuple:
+                minor_states = minor_states.h
+
+        # B x T x minor_size
+        major_inputs = tf.reshape(minor_states, [-1, major_len,
+                                                 minor_cell_size])
+
+        with tf.variable_scope("major_rnn"):
+            if ('dtype' not in kwargs) and ('initial_state' not in kwargs):
+                results = tf.nn.dynamic_rnn(
+                    cell=self._cell,
+                    inputs=major_inputs,
+                    dtype=tf.float32,
+                    **kwargs)
+            else:
+                results = tf.nn.dynamic_rnn(
+                    cell=self._cell,
+                    inputs=major_inputs,
+                    **kwargs)
 
         self._add_internal_trainable_variables()
         # Add trainable variables of `self._cell` which may be constructed
