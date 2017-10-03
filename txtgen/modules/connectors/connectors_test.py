54730ff5ffba1050bd108769b713a330cb466c3c
jxhe <junxianh2@gmail.com>
2017-10-03 02:56:25 -0400

    added concat_connector

--- txtgen/modules/connectors/connectors_test.py
+++ txtgen/modules/connectors/connectors_test.py
@@ -17,8 +17,9 @@ from tensorflow.python.util import nest    # pylint: disable=E0611
 
 from txtgen.core import layers
 from txtgen.modules.connectors.connectors import ConstantConnector
-from txtgen.modules.connectors.connectors import ReparameterizeStochasticConnector
+from txtgen.modules.connectors.connectors import ReparameterizedStochasticConnector
 from txtgen.modules.connectors.connectors import StochasticConnector
+from txtgen.modules.connectors.connectors import ConcatConnector
 
 
 class TestConnectors(tf.test.TestCase):
@@ -60,20 +61,19 @@ class TestConnectors(tf.test.TestCase):
         # TODO(zhiting)
         pass
 
-    def test_reparameterize_stochastic_connector(self): # pylint: disable=too-many-locals
-        """Tests the logic of RepaStochasticConnector.
+    def test_reparameterized_stochastic_connector(self): # pylint: disable=too-many-locals
+        """Tests the logic of ReparameterizedStochasticConnector.
         """
         variable_size = 5
-        ctx_size = 3
 
         # pylint: disable=invalid-name
-        mu = tf.zeros(shape=[self._batch_size, variable_size])
-        var = tf.ones(shape=[self._batch_size, variable_size])
-        gauss_connector = ReparameterizeStochasticConnector(variable_size)
+        mu = tf.zeros(variable_size)
+        var = tf.ones(variable_size)
+        gauss_connector = ReparameterizedStochasticConnector(variable_size)
         gauss_ds = tfds.MultivariateNormalDiag(loc = mu, scale_diag = var)
 
 
-        sample = gauss_connector(gauss_ds)
+        sample = gauss_connector(gauss_ds, self._batch_size)
 
         with self.test_session() as sess:
             sess.run(tf.global_variables_initializer())
@@ -91,6 +91,39 @@ class TestConnectors(tf.test.TestCase):
             for i in range(variable_size):
                self.assertAlmostEqual(0, sample_mu[i], delta=0.2)
                self.assertAlmostEqual(1, sample_var[i], delta=0.2)
+
+    def test_concat_connector(self): # pylint: disable=too-many-locals
+        """Tests the logic of ConcatConnector.
+        """
+        gauss_size = 5
+        constant_size = 7
+        variable_size = 13
+
+        categorical_connector = StochasticConnector(1)
+        gauss_connector = ReparameterizedStochasticConnector(variable_size)
+        constant_connector = ConstantConnector(constant_size)
+        concat_connector = ConcatConnector(variable_size)
+
+
+        # pylint: disable=invalid-name
+        mu = tf.zeros(gauss_size)
+        var = tf.ones(gauss_size)
+        categorical_prob = [0.1, 0.2, 0.7]
+        categorical_ds = tfds.Categorical(probs = categorical_prob)
+        gauss_ds = tfds.MultivariateNormalDiag(loc = mu, scale_diag = var)
+        print(gauss_connector(gauss_ds, self._batch_size).shape)
+        print(categorical_connector(categorical_ds, self._batch_size).shape)
+        print(constant_connector(self._batch_size, value=1.).shape)
+
+        state = concat_connector([gauss_connector(gauss_ds, self._batch_size), categorical_connector(categorical_ds, self._batch_size), constant_connector(self._batch_size, value=1.)])
+
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            output = sess.run(state)
+
+            # check the same size
+            self.assertEqual(output.shape[0], self._batch_size)
+            self.assertEqual(output.shape[1], variable_size)
 
 if __name__ == "__main__":
     tf.test.main()
