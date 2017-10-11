ff0d791bbdf473c24a5c6e42ad8b7156820280e5
Shi Haoran <shore@pku.edu.cn>
2017-10-10 19:25:17 -0700

    AttentionRNNDecoder

--- txtgen/modules/decoders/rnn_decoders.py
+++ txtgen/modules/decoders/rnn_decoders.py
@@ -71,3 +71,71 @@ class BasicRNNDecoder(RNNDecoderBase):
     def output_dtype(self):
         return BasicDecoderOutput(
             rnn_output=dtypes.float32, sample_id=dtypes.int32)
+
+class AttentionRNNDecoder(RNNDecoderBase):
+    """Basic RNN decoder that performs sampling at each step.
+    See :class:`~txtgen.modules.decoders.RNNDecoderBase` for the arguments.
+    """
+
+    def __init__(self,  # pylint: disable=too-many-arguments
+                 mode,
+                 vocab_size,
+                 n_hidden, # num_units: The depth of the attention mechanism.
+                 attention_keys, #encoder_output, [batch_size, max_length, ...]
+                 attention_values, #encoder_output,
+                 attention_values_length,
+                 attention_fn,
+                 reverse_scores_lengths=None,
+                 name = 'attention_rnn_decoder'
+                 cell=None,
+                 embedding=None,
+                 embedding_trainable=True,
+                 hparams=None):
+        AttentionRNNDecoder.__init__(self, cell, embedding, vocab_size, hparams)
+        att_params = hparams['attention']
+        attention_class = get_class(hparams['attention']['class']) #LuongAttention
+        attention_mechanism = attention_class(n_hidden, attention_keys, attention_values_length)
+        attn_cell = get_class('AttentionWrapper')(self._cell, attention_mechanism) # more parameters can be added here
+        self._cell = attn_cell
+
+    @staticmethod
+    def default_hparams():
+        """Returns a dictionary of hyperparameters with default values.
+        The hyperparameters have the same structure as in
+        :meth:`txtgen.modules.RNNDecoderBase.default_hparams`, except that
+        the default "name" is "basic_rnn_decoder".
+        """
+        hparams = RNNDecoderBase.default_hparams()
+        hparams["name"] = "attention_rnn_decoder"
+        return hparams
+
+    def initialize(self, name=None):
+        return self._helper.initialize() + (self._initial_state,)
+
+    def step(self, time, inputs, state, name=None):
+        cell_outputs, cell_state = self._cell(inputs, state)
+        logits = tf.contrib.layers.fully_connected(
+            inputs=cell_outputs, num_outputs=self._vocab_size)
+        sample_ids = self._helper.sample(
+            time=time, outputs=logits, state=cell_state)
+        (finished, next_inputs, next_state) = self._helper.next_inputs(
+            time=time,
+            outputs=logits,
+            state=cell_state,
+            sample_ids=sample_ids)
+        outputs = BasicDecoderOutput(logits, sample_ids)
+        return (outputs, next_state, next_inputs, finished)
+
+    def finalize(self, outputs, final_state, sequence_lengths):
+        return outputs, final_state
+
+    @property
+    def output_size(self):
+        return BasicDecoderOutput(
+            rnn_output=self._vocab_size,
+            sample_id=tensor_shape.TensorShape([]))
+
+    @property
+    def output_dtype(self):
+        return BasicDecoderOutput(
+            rnn_output=dtypes.float32, sample_id=dtypes.int32)
--- txtgen/modules/decoders/rnn_decoders_test.py
+++ txtgen/modules/decoders/rnn_decoders_test.py
@@ -1,4 +1,3 @@
-#
 """
 Unit tests for RNN decoders.
 """
@@ -16,6 +15,7 @@ from tensorflow.contrib.seq2seq import BasicDecoderOutput
 from txtgen.modules.decoders.rnn_decoders import BasicRNNDecoder
 from txtgen.modules.decoders.rnn_decoder_helpers import get_helper
 from txtgen import context
+
 
 class BasicRNNDecoderTest(tf.test.TestCase):
     """Test basic RNN decoder.
@@ -102,6 +102,132 @@ class BasicRNNDecoderTest(tf.test.TestCase):
                 outputs_.sample_id.shape, (self._batch_size, max_length))
             self.assertEqual(final_state_[0].shape,
                              (self._batch_size, cell_dim))
+
+
+class AttentionRNNDecoderTest(tf.test.TestCase):
+    """Test basic RNN decoder.
+    """
+    def setUp(self):
+        tf.test.TestCase.setUp(self)
+        self.attention_dim = 64
+        self.input_seq_len = 10
+    def create_decoder(self, helper, mode):
+        attention_fn = AttentionLayerDot(
+            params={"num_units": self.attention_dim},
+            mode=tf.contrib.learn.ModeKeys.TRAIN)
+        attention_values = tf.convert_to_tensor( 
+            np.random.randn(self.batch_size, self.input_seq_len, 32),
+            dtype=tf.float32)
+        attention_keys = tf.convert_to_tensor(
+            np.random.randn(self.batch_size, self.input_seq_len, 32),
+            dtype=tf.float32)
+        params = AttentionRNNDecoder.default_params()
+        params["max_decode_length"] = self.max_decode_length
+        return AttentionRNNDecoder(
+            params=params,
+            mode=mode,
+            vocab_size=self.vocab_size,
+            attention_keys=attention_keys,
+            attention_values=attention_values,
+            attention_values_length=np.arange(self.batch_size) + 1,
+            attention_fn=attention_fn)
+    def test_attention_scores(self):
+        decoder_output_ = self.test_with_fixed_inputs()
+        np.testing.assert_array_equal(
+            decoder_output_.attention_scores.shape,
+            [self.sequence_length, self.batch_size, self.input_seq_len])
+
+        # Make sure the attention scores sum to 1 for each step
+        scores_sum = np.sum(decoder_output_.attention_scores, axis=2)
+        np.testing.assert_array_almost_equal(
+            scores_sum, np.ones([self.sequence_length, self.batch_size]))
+
+class AttentionRNNDecoderTest(tf.test.TestCase):
+    """Attention RNN decoder.
+    """
+    def setUp(self):
+        tf.test.TestCase.setUp(self)
+        self._vocab_size = 4
+        self._max_time = 8
+        self._batch_size = 16
+        self._inputs = tf.random_uniform([self._batch_size, self._max_time], maxval=self._vocab_size, dtype=tf.int32)
+    def test_decode_train(self):
+        """Tests decoding in training mode.
+        """
+        decoder = AttentionRNNDecoder(vocab_size=self._vocab_size)
+        encoder_output = tf.random.uniform([self._batch_size, self._max_time, 100]) 
+        seq_lenth = np.random.randint(self._max_time, size= [self._batch_size])
+        encoder_values_length = tf.constant(seq_lenth)
+        decoder = AttentionRNNDecoder(vocab_size = self._vocab_size, attention_keys= encoder_output, attention_values = encoder_output, attention_values_length = encoder_values_length)
+
+        helper_train = get_helper(
+            decoder.hparams.helper_train.type,
+            inputs=self._inputs,
+            sequence_length=[self._max_time]*self._batch_size,
+            embedding=decoder.embedding,
+            **decoder.hparams.helper_train.kwargs.todict())
+
+        outputs, final_state, sequence_lengths = decoder(
+            helper_train, decoder.cell.zero_state(self._batch_size, tf.float32))
+
+        # 5 trainable variables: embedding, cell-kernel, cell-bias,
+        # fc-layer-weights, fc-layer-bias
+        self.assertEqual(len(decoder.trainable_variables), 5)
+
+        cell_dim = decoder.hparams.rnn_cell.cell.kwargs.num_units
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            outputs_, final_state_, sequence_lengths_ = sess.run(
+                [outputs, final_state, sequence_lengths],
+                feed_dict={context.is_train(): True})
+            self.assertIsInstance(outputs_, BasicDecoderOutput)
+            self.assertEqual(
+                outputs_.rnn_output.shape,
+                (self._batch_size, self._max_time, self._vocab_size))
+            self.assertEqual(
+                outputs_.sample_id.shape, (self._batch_size, self._max_time))
+            self.assertEqual(final_state_[0].shape,
+                                                          (self._batch_size, cell_dim))
+            np.testing.assert_array_equal(
+                sequence_lengths_,
+                [self._max_time]*self._batch_size)
+
+
+    def test_decode_infer(self):
+        """Tests decoding in inferencee mode.
+        """
+        decoder = BasicRNNDecoder(vocab_size=self._vocab_size)
+
+        helper_infer = get_helper(
+            decoder.hparams.helper_infer.type,
+            embedding=decoder.embedding,
+            start_tokens=[self._vocab_size-2]*self._batch_size,
+            end_token=self._vocab_size-1,
+            **decoder.hparams.helper_train.kwargs.todict())
+
+        outputs, final_state, sequence_lengths = decoder(
+            helper_infer, decoder.cell.zero_state(self._batch_size, tf.float32))
+
+        # 5 trainable variables: embedding, cell-kernel, cell-bias,
+        # fc-layer-weights, fc-layer-bias
+        self.assertEqual(len(decoder.trainable_variables), 5)
+
+        cell_dim = decoder.hparams.rnn_cell.cell.kwargs.num_units
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            outputs_, final_state_, sequence_lengths_ = sess.run(
+                [outputs, final_state, sequence_lengths],
+                feed_dict={context.is_train(): False})
+            self.assertIsInstance(outputs_, BasicDecoderOutput)
+            max_length = max(sequence_lengths_)
+            self.assertEqual(
+                outputs_.rnn_output.shape,
+                (self._batch_size, max_length, self._vocab_size))
+            self.assertEqual(
+                outputs_.sample_id.shape, (self._batch_size, max_length))
+            self.assertEqual(final_state_[0].shape, (self._batch_size, cell_dim))
+
+
 
 if __name__ == "__main__":
     tf.test.main()
