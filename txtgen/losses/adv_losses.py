6d2758895b2b887a7d1e7c550f7dbe4d30afec94
Qizhe Xie <qzxie@Qizhes-MacBook-Pro.local>
2017-10-13 11:22:39 -0400

    independent discriminator

--- txtgen/losses/adv_losses.py
+++ txtgen/losses/adv_losses.py
@@ -9,11 +9,8 @@ from txtgen.context import is_train
 
 def adversarial_losses(true_data,
                        generated_data,
-                       embedding=None,
-                       vocab_size=None,
-                       hparams=None,
+                       discriminator
                        ):
-    discriminator = ForwardRNNEncoder(embedding=embedding, vocab_size=vocab_size, hparams=hparams)
     true_cost = discriminator_result(discriminator, true_data, 1, False)
     fake_cost = discriminator_result(discriminator, generated_data, 0, True)
     disc_cost = tf.reduce_sum(true_cost + fake_cost) #divide by batch size?
@@ -30,50 +27,3 @@ def discriminator_result(discriminator, data, label, reuse=False):
         cost = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=tf.ones_like(logits) * label)
         return cost
 
-'''
-integrate disc optimizer in the loss calculation? 
-
-class AdversarialLosses():
-
-    """Computes the adversarial loss
-    Preliminary version
-    Returns:
-        A single average loss.
-    """
-    def __init__(self,
-                 embedding=None,
-                 vocab_size=None,
-                 hparams=None,
-                 ):
-        self.true_data = tf.placeholder(tf.int32, name="true_data")
-        self.generated_data = tf.placeholder(tf.int32, name="generated_data")
-        self.true_data_sequence_length = tf.placeholder(tf.int32, name="true_data_sequence_length")
-        self.generated_data_sequence_length = tf.placeholder(tf.int32, name="generated_data_sequence_length")
-        self.discriminator = ForwardRNNEncoder(embedding=embedding, vocab_size=vocab_size, hparams=hparams)
-        true_cost = self.discriminator_result(self.true_data, 1, False)
-        self.generate_cost = self.discriminator_result(self.generated_data, 0, True)
-        self.discriminator_cost = tf.reduce_sum(true_cost + self.generate_cost)
-        self.disc_train_op, self.disc_global_step = opt.get_train_op(self.discriminator_cost)
-
-    def discriminator_result(self, data, label, reuse=False):
-        """Loss for both generated data and true data
-        """
-        _, state = self.discriminator(data)
-        with tf.variable_scope('discriminator', reuse=reuse):
-            logits = tf.layers.dense(state, 1)
-            cost = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=tf.ones_like(logits) * label)
-            return cost
-
-    def adversarial_losses(self,
-                           true_data,
-                           generated_data,
-                           ):
-        # pylint: disable=not-context-manager
-        if is_train():
-            with tf.Session() as sess:
-                # train optimizer
-                sess.run([self.disc_train_op, self.disc_global_step, self.discriminator_cost],
-                    feed_dict={self.true_data: true_data, self.generated_data: generated_data})
-        with tf.Session() as sess:
-            generator_cost = sess.run([self.generate_cost], feed_dict={self.generated_data: generated_data})
-        return generator_cost'''
