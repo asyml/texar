29f158d9ab5f4dd45f592d4e816b772fb2007fb8
Zhiting Hu <zhitinghu@gmail.com>
2017-11-15 17:13:19 -0500

    adding discriminators, cnn_encoders
    Former-commit-id: f50d07714ae65891400bedb70e14cf8200dddb19

---
+++ txtgen/modules/predictors/discriminators.py
@@ -0,0 +1,19 @@
+#
+"""
+Discriminators.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from txtgen.modules.module_base import ModuleBase
+
+__all__ = [
+]
+
+# TODO(zhiting): Discriminators should be based on Encoders, e.g., takes in
+# an encoder as `feature_extractor`, and add an extra classification layer
+# on the encoder outputs.
+class Discriminator(ModuleBase):
+    raise NotImplementedError
