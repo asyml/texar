9ae5f4d0761d443c87ac974c01882c2be54569a5
Zichao Yang <zichaoy@cs.cmu.edu>
2017-12-21 15:47:19 -0500

    decoder helper gumbel and softmax
    Former-commit-id: 19c150cb47da957c1c186bdb5d6c977796c3a12b

--- texar/modules/decoders/rnn_decoder_helpers.py
+++ texar/modules/decoders/rnn_decoder_helpers.py
@@ -9,6 +9,9 @@ from __future__ import print_function
 
 import tensorflow as tf
 from tensorflow.contrib.seq2seq import TrainingHelper as TFTrainingHelper
+from tensorflow.contrib.seq2seq import Helper as TFHelper
+from tensorflow.contrib.distributions import RelaxedOneHotCategorical \
+    as GumbelSoftmax
 
 from texar.core import utils
 
@@ -155,3 +158,97 @@ class EmbeddingTrainingHelper(TFTrainingHelper):
                 sequence_length=sequence_length,
                 time_major=time_major,
                 name=name)
+
+
+class SoftmaxEmbeddingHelper(TFHelper):
+    """A helper that feed softmax to the next step.
+
+    Use the softmax probability to pass through an embedding layer to get the
+    next input.
+    """
+
+    def __init__(self, embedding, start_tokens, tau, stop_gradient=False):
+        """Initializer.
+
+        Args:
+          embedding: A callable that takes a vector tensors of 'ids' or the
+            params argument for embedding_lookup.
+          start_tokens: 'int32' vector shaped [batch_size], the start tokens.
+          tau: softmax anneal temperature.
+          stop_gradient: stop the gradient when feeding to the next step.
+        """
+
+        if callable(embedding):
+            self._embedding_fn = embedding
+        else:
+            self._embedding = embedding
+            self._embedding_fn = (
+                lambda ids: tf.nn.embedding_lookup(embedding, ids))
+
+        self._start_tokens = tf.convert_to_tensor(
+            start_tokens, dtype=tf.int32, name="start_tokens")
+        self._start_inputs = self._embedding_fn(self._start_tokens)
+        self._batch_size = tf.size(self._start_tokens)
+        self._tau = tau
+        self._stop_gradient = stop_gradient
+
+    @property
+    def batch_size(self):
+        return self._batch_size
+
+    @property
+    def sample_ids_dtype(self):
+        return tf.float32
+
+    @property
+    def sample_ids_shape(self):
+        return self._embedding.get_shape()[:1]
+
+    def initialize(self, name=None):
+        finished = tf.tile([False], [self._batch_size])
+        return (finished, self._start_inputs)
+
+    def sample(self, time, outputs, state, name=None):
+        sample_ids = tf.nn.softmax(outputs / self._tau)
+        return sample_ids
+
+    def next_inputs(self, time, outputs, state, sample_ids, name=None):
+        finished = [False] * self._batch_size
+        if self._stop_gradient:
+            sample_ids = tf.stop_gradient(sample_ids)
+        next_inputs = tf.matmul(sample_ids, self._embedding)
+        return (finished, next_inputs, state)
+
+class GumbelSoftmaxEmbeddingHelper(SoftmaxEmbeddingHelper):
+    """A helper that use Gumbel Softmax sampling.
+
+    Use the Gumbel Softmax sample and pass the sample through an embedding
+    layer to get the next input.
+    """
+
+    def __init__(self, embedding, start_tokens, tau, straight_through=False,
+                 stop_gradient=False):
+        """Initializer.
+
+        Args:
+          embedding: A callabel that takes a vector tensors of 'ids' or the
+            params argument for embedding_lookup.
+          start_tokens: 'int32' vector shaped [batch_size], the start tokens.
+          tau: anneal temperature for sampling.
+          straight_through: whether to use straight_through estimator.
+          stop_gradient: stop the gradients when feeding to the next step.
+        """
+
+        super(GumbelSoftmaxEmbeddingHelper, self).__init__(
+            embedding, start_tokens, tau, stop_gradient)
+        self._straight_through = straight_through
+
+    def sample(self, time, outputs, state, name=None):
+        sample_ids = GumbelSoftmax(self._tau, outputs).sample()
+        if self._straight_through:
+            size = tf.shape(sample_ids)[-1]
+            sample_ids_hard = tf.cast(
+                tf.one_hot(tf.argmax(sample_ids, -1), size), sample_ids.dtype)
+            sample_ids = tf.stop_gradient(sample_ids_hard - sample_ids) \
+                         + sample_ids
+        return sample_ids
