ff7d44d79f02717d736428d52bc910c2898b9474
shore <shore@pku.edu.cn>
2017-12-21 15:21:31 +0800

    update
    Former-commit-id: c25db37fbcdc809363d7e7df74a99c81df887d24

--- examples/transformer.py
+++ examples/transformer.py
@@ -26,34 +26,32 @@ if __name__ == "__main__":
         "batch_size": 32,
         "shuffle": True,
         "source_dataset": {
-            "files": ['data/translation/de-en/train_de_sentences.txt'],
-            "vocab_file": 'data/translation/de-en/filter_de.vocab.txt',
+            "files": ['~/t2t_data/train_ende_wmt32k_en'],
+            "vocab_file": '~/t2t_data/vocab.ende.32768.filtered',
             "processing": {
-                "bos_token": "<S>",
-                "eos_token": "</S>",
+                "bos_token": "<BOS>",
+                "eos_token": "<EOS>",
             }
         },
         "target_dataset": {
-            "files": ['data/translation/de-en/train_en_sentences.txt'],
-            "vocab_file": 'data/translation/de-en/filter_en.vocab.txt',
+            "files": ['~/t2t_data/train_ende_wmt32k_de'],
+            "vocab_share":True,
             "processing":{
-                "bos_token": "<S>",
-                "eos_token": "</S>",
+                "bos_token": "<BOS>",
+                "eos_token": "<EOS>",
             },
         }
     }
     extra_hparams = {
-        'max_seq_length':10,
-        'scale':True,
-        'sinusoid':False,
+        'max_seq_length':1000000000,
         'embedding': {
             'name': 'lookup_table',
             'dim': 512,
             'initializer': {
                 'type': tf.contrib.layers.xavier_initializer(),
             },
-            'trainable':True,
         },
+        'sinusoid': True,
         'num_blocks': 6,
         'num_heads': 8,
         'poswise_feedforward': {
@@ -84,13 +82,8 @@ if __name__ == "__main__":
     text_data_batch = text_database()
     ori_src_text = text_data_batch['source_text_ids']
     ori_tgt_text = text_data_batch['target_text_ids']
-
-    padded_src_text = tf.concat([ori_src_text, tf.zeros([tf.shape(ori_src_text)[0],\
-        extra_hparams['max_seq_length']+1-tf.shape(ori_src_text)[1]], dtype=tf.int64)], axis=1)
-    padded_tgt_text = tf.concat([ori_tgt_text, tf.zeros([tf.shape(ori_tgt_text)[0],\
-        extra_hparams['max_seq_length']+1-tf.shape(ori_tgt_text)[1]], dtype=tf.int64)], axis=1)
-    encoder_input = padded_src_text[:, 1:]
-    decoder_input = padded_tgt_text[:, :-1]
+    encoder_input = ori_src_text[:, 1:]
+    decoder_input = ori_tgt_text[:, :-1]
     encoder = TransformerEncoder(vocab_size=text_database.source_vocab.vocab_size,\
         hparams=extra_hparams)
     encoder_output = encoder(encoder_input)
@@ -101,9 +94,7 @@ if __name__ == "__main__":
     loss_params = {
         'label_smoothing':0.1,
     }
-
-    labels = padded_tgt_text[:, 1:]
-
+    labels = ori_tgt_text[:, 1:]
     smooth_labels = mle_losses.label_smoothing(labels, text_database.target_vocab.vocab_size, \
         loss_params['label_smoothing'])
     mle_loss = mle_losses.average_sequence_softmax_cross_entropy(
@@ -126,9 +117,7 @@ if __name__ == "__main__":
     train_op, global_step = opt.get_train_op(mle_loss, hparams=opt_hparams)
     merged = tf.summary.merge_all()
     saver = tf.train.Saver(max_to_keep=10)
-
     with tf.Session() as sess:
-
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
         sess.run(tf.tables_initializer())
@@ -142,7 +131,7 @@ if __name__ == "__main__":
                     [encoder_input, labels, preds, train_op, global_step, mle_loss, merged],
                     feed_dict={context.is_train(): True})
                 writer.add_summary(mgd, global_step=step)
-                if step % 1703 == 0:
+                if step % 100 == 0:
                     print('step:{} loss:{}'.format(step, loss))
                     saver.save(sess, './logdir/my-model', global_step=step)
         except tf.errors.OutOfRangeError:
