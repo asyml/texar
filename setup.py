f6fa9a32b27c15b5a9507071e5ad9336a12c13c0
Zhiting Hu <zhitinghu@gmail.com>
2018-03-16 16:16:04 -0400

    updated tf version to 1.6

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setuptools.setup(
         'numpy'
     ],
     extras_require={
-        'tensorflow': ['tensorflow>=1.4.0'],
-        'tensorflow with gpu': ['tensorflow-gpu>=1.4.0']
+        'tensorflow': ['tensorflow>=1.6.0'],
+        'tensorflow with gpu': ['tensorflow-gpu>=1.6.0']
     },
     classifiers=[
         'Intended Audience :: Developers',
