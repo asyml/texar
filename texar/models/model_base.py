770d876d9063eda80d2c0bd67dffa1409c76ad62
Zhiting Hu <zhitinghu@gmail.com>
2018-03-17 21:26:35 -0400

    updated ModelBase

--- texar/models/model_base.py
+++ texar/models/model_base.py
@@ -39,8 +39,32 @@ class ModelBase(ModuleBase):
         """
         raise NotImplementedError
 
-    def get_train_op(self):
+    def get_train_op(self, loss, variables=None, global_step=None,
+                     increment_global_step=True):
         """Creates the train op of the model.
+
+        Args:
+            loss: A `Tensor` of the model loss.
+            variables (list of Variables, optional): Variables to optimize. If
+                `None`, the model variables are used.
+            global_step (scalar int Tensor, optional): step counter to update
+                on each step unless :attr:`increment_global_step` is `False`.
+                If `None`, a new global step variable will be created.
+            incremental_global_step (bool): Whether to increment
+                :attr:`global_step`. This is useful if the :attr:`global_step`
+                is used in multiple training ops per training step
+                (e.g. to optimize different parts of the model) to avoid
+                incrementing :attr:`global_step` more times than necessary.
+
+        Returns:
+            tuple: (train_op, global_step). If :attr:`global_step` is
+            provided, the same :attr:`global_step` variable is returned,
+            otherwise a new global step is created and returned.
         """
-        raise NotImplementedError
+        return core.get_train_op(
+            loss,
+            variables=variables or self._trainable_variables,
+            global_step=global_step,
+            increment_global_step=increment_global_step,
+            hparams=self._hparams)
 
