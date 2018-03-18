e70906758f1a32cfaa7b20dfc26fb0cdde64f6dc
Zhiting Hu <zhitinghu@gmail.com>
2018-03-17 21:11:26 -0400

    added ModelBase
    Former-commit-id: a4fa8f2d46e63c6d8156b97967001a539283d098

--- texar/models/model_base.py
+++ texar/models/model_base.py
@@ -7,9 +7,35 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from texar.modules.module_base import ModuleBase
+from texar.module_base import ModuleBase
+from texar import core
 
 __all__ = [
+    "ModelBase"
 ]
 
-#class EncoderBase(ModuleBase):
+class ModelBase(ModuleBase):
+    """Base class inherited by all model classes.
+    """
+
+    def __init__(self, hparams=None):
+        ModuleBase.__init__(self, hparams)
+
+    @staticmethod
+    def default_hparams():
+        """Returns a dictionary of hyperparameters with default values.
+        """
+        hparams = core.default_optimization_hparams()
+        hparams.update({"name": "model"})
+        return hparams
+
+    def _build(self, *args, **kwargs):
+        """The model logic.
+        """
+        raise NotImplementedError
+
+    def get_train_op(self):
+        """Creates the train op of the model.
+        """
+        raise NotImplementedError
+
