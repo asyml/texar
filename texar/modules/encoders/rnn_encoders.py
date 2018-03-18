da37438735fd4b845bb0874562bd071865c480bb
Zhiting Hu <zhitinghu@gmail.com>
2018-03-17 22:01:57 -0400

    removed embeddings in rnn_encoders
    Former-commit-id: b64f925f978bdbec1b3927df44448b9a54c0f87c

--- texar/modules/encoders/rnn_encoders.py
+++ texar/modules/encoders/rnn_encoders.py
@@ -11,12 +11,12 @@ import tensorflow as tf
 
 from texar.modules.encoders.encoder_base import EncoderBase
 from texar.core import layers
-from texar.modules.embedders import embedder_utils
 
 # pylint: disable=not-context-manager, too-many-arguments
 
 __all__ = [
-    "RNNEncoderBase", "UnidirectionalRNNEncoder",
+    "RNNEncoderBase",
+    "UnidirectionalRNNEncoder",
     "BidirectionalRNNEncoder"
 ]
 
@@ -24,52 +24,13 @@ class RNNEncoderBase(EncoderBase):
     """Base class for all RNN encoder classes.
 
     Args:
-        embedding (optional): A `Variable` or a 2D `Tensor` (or `array`)
-            of shape `[vocab_size, embedding_dim]` that contains the token
-            embeddings. Ignore if :attr:`hparams["use_embedding"]`
-            is `False`. If :attr:`hparams["use_embedding"]` is `True`:
-
-            If a `Variable`, it is directly used in encoding, and
-            hyperparameters in :attr:`hparams["embedding"]` are ignored.
-
-            If a `Tensor` or `array`, it is used to initialize the token
-            embedding variable. The :attr:`"initializer"` and :attr:`"dim"`
-            hyperparameters in :attr:`hparams["embedding"]` are ignored.
-
-            If not given, a new `Variable` is created as specified in
-            :attr:`hparams["embedding"]`.
-        vocab_size (int, optional): The vocabulary size. Required if
-            :attr:`hparams["use_embedding"]` is `True` (default) and
-            :attr:`embedding` is not provided.
         hparams (dict, optional): Encoder hyperparameters. If it is not
             specified, the default hyperparameter setting is used. See
             :attr:`default_hparams` for the sturcture and default values.
     """
 
-    def __init__(self,
-                 embedding=None,
-                 vocab_size=None,
-                 hparams=None):
+    def __init__(self, hparams=None):
         EncoderBase.__init__(self, hparams)
-
-        # Make embedding
-        self._vocab_size = vocab_size
-        self._embedding = None
-        if self._hparams.use_embedding:
-            if embedding is None and self._vocab_size is None:
-                raise ValueError(
-                    "`vocab_szie` is required if embedding is enabled and "
-                    "`embedding` is not provided")
-            if isinstance(embedding, tf.Variable):
-                self._embedding = embedding
-            else:
-                self._embedding = embedder_utils.get_embedding(
-                    self._hparams.embedding, embedding, self._vocab_size,
-                    self.variable_scope)
-            if self._hparams.embedding.trainable:
-                self._add_trainable_variable(self._embedding)
-            if self._vocab_size is None:
-                self._vocab_size = self._embedding.get_shape().as_list()[0]
 
     @staticmethod
     def default_hparams():
@@ -79,41 +40,15 @@ class RNNEncoderBase(EncoderBase):
             .. code-block:: python
 
                 {
-                    "use_embedding": True,
-                    "embedding": texar.core.layers.default_embedding_hparams(),
-                    "name": "unidirectional_rnn_encoder"
+                    "name": "rnn_encoder"
                 }
 
             Here:
-
-            "use_embedding" : bool
-                Whether token embedding is used.
-
-                If `True` (default), input to the encoder can be integer
-                indexes and will be used to look up the embedding vectors.
-                If `False`, the input is directly fed into the RNN to encode.
-
-            "embedding" : dict
-                A dictionary of token embedding hyperparameters for
-                embedding initialization.
-
-                Ignored if :attr:`"use_embedding"` is `False`, or if
-                :attr:`embedding` is given and is `Variable` when
-                constructing the decoder.
-
-                If :attr:`embedding` is given and is a Tensor or array, the
-                "dim" and "initializer" specs in the embedding hyperparameterrs
-                are ignored.
-
-                The default value is defined in
-                :meth:`~texar.core.layers.default_embedding_hparams`.
 
             "name" : str
                 Name of the encoder.
         """
         return {
-            "use_embedding": True,
-            "embedding": embedder_utils.default_embedding_hparams(),
             "name": "rnn_encoder"
         }
 
@@ -130,12 +65,6 @@ class RNNEncoderBase(EncoderBase):
         """
         raise NotImplementedError
 
-    @property
-    def embedding(self):
-        """The embedding variable.
-        """
-        return self._embedding
-
 
 class UnidirectionalRNNEncoder(RNNEncoderBase):
     """One directional RNN encoder.
@@ -143,23 +72,6 @@ class UnidirectionalRNNEncoder(RNNEncoderBase):
     Args:
         cell: (RNNCell, optional) If it is not specified,
             a cell is created as specified in :attr:`hparams["rnn_cell"]`.
-        embedding (optional): A `Variable` or a 2D `Tensor` (or `array`)
-            of shape `[vocab_size, embedding_dim]` that contains the token
-            embeddings. Ignore if :attr:`hparams["use_embedding"]`
-            is `False`. If :attr:`hparams["use_embedding"]` is `True`:
-
-            If a `Variable`, it is directly used in encoding, and
-            hyperparameters in :attr:`hparams["embedding"]` are ignored.
-
-            If a `Tensor` or `array`, it is used to initialize the token
-            embedding variable. The :attr:`"initializer"` and :attr:`"dim"`
-            hyperparameters in :attr:`hparams["embedding"]` are ignored.
-
-            If not given, a new `Variable` is created as specified in
-            :attr:`hparams["embedding"]`.
-        vocab_size (int, optional): The vocabulary size. Required if
-            :attr:`hparams["use_embedding"]` is `True` (default) and
-            :attr:`embedding` is not provided.
         hparams (dict, optional): Encoder hyperparameters. If it is not
             specified, the default hyperparameter setting is used. See
             :attr:`default_hparams` for the sturcture and default values.
@@ -167,10 +79,8 @@ class UnidirectionalRNNEncoder(RNNEncoderBase):
 
     def __init__(self,
                  cell=None,
-                 embedding=None,
-                 vocab_size=None,
                  hparams=None):
-        RNNEncoderBase.__init__(self, embedding, vocab_size, hparams)
+        RNNEncoderBase.__init__(self, hparams)
 
         # Make RNN cell
         with tf.variable_scope(self.variable_scope):
@@ -188,8 +98,6 @@ class UnidirectionalRNNEncoder(RNNEncoderBase):
 
                 {
                     "rnn_cell": default_rnn_cell_hparams(),
-                    "use_embedding": True,
-                    "embedding": default_embedding_hparams(),
                     "name": "unidirectional_rnn_encoder"
                 }
 
@@ -202,28 +110,6 @@ class UnidirectionalRNNEncoder(RNNEncoderBase):
                 The default value is defined in
                 :meth:`~texar.core.layers.default_rnn_cell_hparams`.
 
-            "use_embedding" : bool
-                Whether token embedding is used.
-
-                If `True` (default), input to the encoder should contain integer
-                indexes and will be used to look up the embedding vectors.
-                If `False`, the input is directly fed into the RNN to encode.
-
-            "embedding" : dict
-                A dictionary of token embedding hyperparameters for
-                embedding initialization.
-
-                Ignored if :attr:`"use_embedding"` is `False`, or if
-                :attr:`embedding` is given and is `Variable` when
-                constructing the decoder.
-
-                If :attr:`embedding` is given and is a Tensor or array, the
-                "dim" and "initializer" specs in the embedding hyperparameterrs
-                are ignored.
-
-                The default value is defined in
-                :meth:`~texar.core.layers.default_embedding_hparams`.
-
             "name" : str
                 Name of the encoder
         """
@@ -233,27 +119,18 @@ class UnidirectionalRNNEncoder(RNNEncoderBase):
         return hparams
 
     def _build(self, inputs, sequence_length=None, initial_state=None,
-               use_embedding=None, **kwargs):
+               **kwargs):
         """Encodes the inputs.
 
         Args:
-            inputs: If embedding is enabled, this must be a 2D Tensor of shape
-                `[batch_size, max_time]` containing input sequences of integer
-                token indexes. Otherwise, this must be a 3D Tensor of shape
-                `[batch_size, max_time, dim]`. The first two dimensions
+            inputs: A 3D Tensor of shape `[batch_size, max_time, dim]`.
+                The first two dimensions
                 `batch_size` and `max_time` may be exchanged if
                 `time_major=True` is specified.
             sequence_length (int list or 1D Tensor, optional): Sequence lengths
                 of the batch inputs. Used to copy-through state and zero-out
                 outputs when past a batch element's sequence length.
             initial_state (optional): Initial state of the RNN.
-            use_embedding (bool, optional): Whether token embedding is used. If
-                `None` (default), the value is set to
-                :attr:`hparams["use_embedding"]`.
-                If `True`, input to the encoder should contain integer
-                indexes and will be used to look up the embedding vectors. In
-                this case, :attr:`hparams["use_embedding"]` must also be `True`.
-                If `False`, the input is directly fed into the RNN to encode.
             **kwargs: Optional keyword arguments of
                 :tf_main:`tf.nn.dynamic_rnn <nn/dynamic_rnn>`,
                 such as `time_major`, `dtype`, etc.
@@ -262,23 +139,10 @@ class UnidirectionalRNNEncoder(RNNEncoderBase):
             Outputs and final state of the encoder.
         """
         #TODO(zhiting): add docs of 'Returns'
-        if use_embedding is None:
-            use_embedding = self.hparams["use_embedding"]
-        if use_embedding:
-            if self._embedding is None:
-                if not self.hparams["use_embedding"]:
-                    raise ValueError(
-                        'Cannot use embedding as hparams["use_embedding"] is '
-                        'set to False.')
-                raise ValueError("Embedding is None.")
-            embedded_inputs = tf.nn.embedding_lookup(self._embedding, inputs)
-        else:
-            embedded_inputs = inputs
-
         if ('dtype' not in kwargs) and (initial_state is None):
             outputs, state = tf.nn.dynamic_rnn(
                 cell=self._cell,
-                inputs=embedded_inputs,
+                inputs=inputs,
                 sequence_length=sequence_length,
                 initial_state=initial_state,
                 dtype=tf.float32,
@@ -286,7 +150,7 @@ class UnidirectionalRNNEncoder(RNNEncoderBase):
         else:
             outputs, state = tf.nn.dynamic_rnn(
                 cell=self._cell,
-                inputs=embedded_inputs,
+                inputs=inputs,
                 sequence_length=sequence_length,
                 initial_state=initial_state,
                 **kwargs)
@@ -322,23 +186,6 @@ class BidirectionalRNNEncoder(RNNEncoderBase):
     Args:
         cell: (RNNCell, optional) If it is not specified,
             a cell is created as specified in :attr:`hparams["rnn_cell"]`.
-        embedding (optional): A `Variable` or a 2D `Tensor` (or `array`)
-            of shape `[vocab_size, embedding_dim]` that contains the token
-            embeddings. Ignore if :attr:`hparams["use_embedding"]`
-            is `False`. If :attr:`hparams["use_embedding"]` is `True`:
-
-            If a `Variable`, it is directly used in encoding, and
-            hyperparameters in :attr:`hparams["embedding"]` are ignored.
-
-            If a `Tensor` or `array`, it is used to initialize the token
-            embedding variable. The :attr:`"initializer"` and :attr:`"dim"`
-            hyperparameters in :attr:`hparams["embedding"]` are ignored.
-
-            If not given, a new `Variable` is created as specified in
-            :attr:`hparams["embedding"]`.
-        vocab_size (int, optional): The vocabulary size. Required if
-            :attr:`hparams["use_embedding"]` is `True` (default) and
-            :attr:`embedding` is not provided.
         hparams (dict, optional): Encoder hyperparameters. If it is not
             specified, the default hyperparameter setting is used. See
             :attr:`default_hparams` for the sturcture and default values.
@@ -347,10 +194,8 @@ class BidirectionalRNNEncoder(RNNEncoderBase):
     def __init__(self,
                  cell_fw=None,
                  cell_bw=None,
-                 embedding=None,
-                 vocab_size=None,
                  hparams=None):
-        RNNEncoderBase.__init__(self, embedding, vocab_size, hparams)
+        RNNEncoderBase.__init__(self, hparams)
 
         # Make RNN cells
         with tf.variable_scope(self.variable_scope):
@@ -377,8 +222,6 @@ class BidirectionalRNNEncoder(RNNEncoderBase):
                     "rnn_cell_fw": default_rnn_cell_hparams(),
                     "rnn_cell_bw": default_rnn_cell_hparams(),
                     "rnn_cell_share_config": True
-                    "use_embedding": True,
-                    "embedding": default_embedding_hparams(),
                     "name": "bidirectional_rnn_encoder"
                 }
 
@@ -406,28 +249,6 @@ class BidirectionalRNNEncoder(RNNEncoderBase):
 
                 If `True` (default), :attr:`"rnn_cell_bw"` is ignored.
 
-            "use_embedding" : bool
-                Whether token embedding is used.
-
-                If `True` (default), input to the encoder can be integer
-                indexes and will be used to look up the embedding vectors.
-                If `False`, the input is directly fed into the RNN to encode.
-
-            "embedding" : dict
-                A dictionary of token embedding hyperparameters for
-                embedding initialization.
-
-                Ignored if :attr:`"use_embedding"` is `False`, or if
-                :attr:`embedding` is given and is `Variable` when
-                constructing the decoder.
-
-                If :attr:`embedding` is given and is a Tensor or array, the
-                "dim" and "initializer" specs in the embedding hyperparameterrs
-                are ignored.
-
-                The default value is defined in
-                :meth:`~texar.core.layers.default_embedding_hparams`.
-
             "name" : str
                 Name of the encoder
         """
@@ -438,28 +259,19 @@ class BidirectionalRNNEncoder(RNNEncoderBase):
         hparams["name"] = "bidirectional_rnn_encoder"
         return hparams
 
-    def _build(self, inputs, sequence_length=None, initial_state_fw=None,
-               initial_state_bw=None, use_embedding=None, **kwargs):
+    def _build(self, inputs, sequence_length=None,
+               initial_state_fw=None, initial_state_bw=None, **kwargs):
         """Encodes the inputs.
 
         Args:
-            inputs: If embedding is enabled, this must be a 2D Tensor of shape
-                `[batch_size, max_time]` containing input sequences of integer
-                token indexes. Otherwise, this must be a 3D Tensor of shape
-                `[batch_size, max_time, dim]`. The first two dimensions
+            inputs: A 3D Tensor of shape `[batch_size, max_time, dim]`.
+                The first two dimensions
                 `batch_size` and `max_time` may be exchanged if
                 `time_major=True` is specified.
             sequence_length (int list or 1D Tensor, optional): Sequence lengths
                 of the batch inputs. Used to copy-through state and zero-out
                 outputs when past a batch element's sequence length.
             initial_state (optional): Initial state of the RNN.
-            use_embedding (bool, optional): Whether token embedding is used. If
-                `None` (default), the value is set to
-                :attr:`hparams["use_embedding"]`.
-                If `True`, input to the encoder should contain integer
-                indexes and will be used to look up the embedding vectors. In
-                this case, :attr:`hparams["use_embedding"]` must also be `True`.
-                If `False`, the input is directly fed into the RNN to encode.
             **kwargs: Optional keyword arguments of
                 :tf_main:`tf.nn.dynamic_rnn <nn/dynamic_rnn>`,
                 such as `time_major`, `dtype`, etc.
@@ -468,25 +280,12 @@ class BidirectionalRNNEncoder(RNNEncoderBase):
             Outputs and final state of the encoder.
         """
         #TODO(zhiting): add docs of 'Returns'
-        if use_embedding is None:
-            use_embedding = self.hparams["use_embedding"]
-        if use_embedding:
-            if self._embedding is None:
-                if not self.hparams["use_embedding"]:
-                    raise ValueError(
-                        'Cannot use embedding as hparams["use_embedding"] is '
-                        'set to False.')
-                raise ValueError("Embedding is None.")
-            embedded_inputs = tf.nn.embedding_lookup(self._embedding, inputs)
-        else:
-            embedded_inputs = inputs
-
         no_initial_state = initial_state_fw is None and initial_state_bw is None
         if ('dtype' not in kwargs) and no_initial_state:
             outputs, output_states = tf.nn.bidirectional_dynamic_rnn(
                 cell_fw=self._cell_fw,
                 cell_bw=self._cell_bw,
-                inputs=embedded_inputs,
+                inputs=inputs,
                 sequence_length=sequence_length,
                 initial_state_fw=initial_state_fw,
                 initial_state_bw=initial_state_bw,
@@ -496,7 +295,7 @@ class BidirectionalRNNEncoder(RNNEncoderBase):
             outputs, output_states = tf.nn.bidirectional_dynamic_rnn(
                 cell_fw=self._cell_fw,
                 cell_bw=self._cell_bw,
-                inputs=embedded_inputs,
+                inputs=inputs,
                 sequence_length=sequence_length,
                 initial_state_fw=initial_state_fw,
                 initial_state_bw=initial_state_bw,
