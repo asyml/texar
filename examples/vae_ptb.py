95ae9580c28469c25fb88c49471a141b090ec7ca
jxhe <junxianh2@gmail.com>
2018-04-07 21:49:15 -0400

    add dropout, 102.5 test nll
    Former-commit-id: 8b4b44c929cbd3e9e21db0c45cd13cd977c8a7cd

--- examples/vae_ptb.py
+++ examples/vae_ptb.py
@@ -49,13 +49,12 @@ import texar as tx
 num_epochs = 30
 hidden_size = 256
 keep_prob = 0.5
-batch_size = 30
-emb_size = 256
-new_kl_rate = 0
+batch_size = 32
+emb_size = 300
 
-annealing_rate = 1.0 / 7000
+annealing_rate = 1.0 / 10000
 
-latent_dims = 64
+latent_dims = 16
 
 cell_hparams = {
     "type": "LSTMBlockCell",
@@ -71,8 +70,9 @@ emb_hparams = {
     "dim": emb_size
 }
 
-connector_hparams = {
-
+anneal_hparams = {
+        "x0": 2500,
+        "k": 0.0025
 }
 
 train_data_hparams = {
@@ -104,37 +104,37 @@ test_data_hparams = {
     }
 }
 
-opt_hparams = {
-    "optimizer": {
-        "type": "GradientDescentOptimizer",
-        "kwargs": {"learning_rate": 1.0}
-    },
-    "gradient_clip": {
-        "type": "clip_by_global_norm",
-        "kwargs": {"clip_norm": 5.}
-    },
-    "learning_rate_decay": {
-        "type": "exponential_decay",
-        "kwargs": {
-            "decay_steps": 1,
-            "decay_rate": 0.5,
-            "staircase": True
-        },
-        "start_decay_step": 3
-    }
-}
-
 # opt_hparams = {
 #     "optimizer": {
-#         "type": "AdamOptimizer",
-#         "kwargs": {"learning_rate": 0.01}
+#         "type": "GradientDescentOptimizer",
+#         "kwargs": {"learning_rate": 1.0}
 #     },
-
 #     "gradient_clip": {
 #         "type": "clip_by_global_norm",
 #         "kwargs": {"clip_norm": 5.}
 #     },
+#     "learning_rate_decay": {
+#         "type": "exponential_decay",
+#         "kwargs": {
+#             "decay_steps": 1,
+#             "decay_rate": 0.5,
+#             "staircase": True
+#         },
+#         "start_decay_step": 3
+#     }
 # }
+
+opt_hparams = {
+    "optimizer": {
+        "type": "AdamOptimizer",
+        "kwargs": {"learning_rate": 0.001}
+    },
+
+    "gradient_clip": {
+        "type": "clip_by_global_norm",
+        "kwargs": {"clip_norm": 5.}
+    },
+}
 
 def kl_dvg(means, logvars):
     kl_cost = -0.5 * (logvars - tf.square(means) -
@@ -142,6 +142,10 @@ def kl_dvg(means, logvars):
     kl_cost = tf.reduce_mean(kl_cost, 0)
 
     return tf.reduce_sum(kl_cost)
+
+def kl_anneal_function(step, k, x0):
+    return 1.0 / (1 + np.exp(-k*(step-x0)))
+
 
 def _main(_):
     # Data
@@ -163,26 +167,23 @@ def _main(_):
     embedder = tx.modules.WordEmbedder(
         vocab_size=train_data.vocab.size, hparams=emb_hparams)
 
-    # if config.keep_prob < 1:
-    #     emb_inputs = tf.nn.dropout(
-    #         emb_inputs, tx.utils.switch_dropout(config.keep_prob))
-    #     emb_outputs = tf.nn.dropout(
-    #         emb_outputs, tx.utils.switch_dropout(config.keep_prob))
 
     encoder = tx.modules.UnidirectionalRNNEncoder(
         hparams={"rnn_cell": cell_hparams})
     input_embedding = embedder(data_batch["text_ids"])
+    output_embedding = embedder(data_batch["text_ids"])
+
     if keep_prob < 1:
-        input_embedding = tf.nn.dropout(
-            input_embedding, tx.utils.switch_dropout(keep_prob))
+        output_embedding = tf.nn.dropout(
+            output_embedding, tx.utils.switch_dropout(keep_prob))
 
     decoder = tx.modules.BasicRNNDecoder(
         vocab_size=train_data.vocab.size, hparams={"rnn_cell": cell_hparams})
 
     connector_mlp = tx.modules.connectors.MLPTransformConnector(
-            latent_dims * 2, hparams={"activation_fn": "relu"})
+            latent_dims * 2)
     connector_stoch = tx.modules.connectors.ReparameterizedStochasticConnector(
-            decoder.cell.state_size, hparams={"activation_fn": "relu"})
+            decoder.cell.state_size)
 
     # initial_state = decoder.zero_state(batch_size, tf.float32)
     _, ecdr_states = encoder(
@@ -201,9 +202,8 @@ def _main(_):
     outputs, final_state, seq_lengths = decoder(
         initial_state=dcdr_states,
         decoding_strategy="train_greedy",
-        inputs=data_batch["text_ids"],
-        sequence_length=data_batch["length"]-1,
-        embedding=embedder)
+        inputs=output_embedding,
+        sequence_length=data_batch["length"]-1)
 
     # Losses & train ops
     mle_loss = tx.losses.sequence_sparse_softmax_cross_entropy(
@@ -219,13 +219,13 @@ def _main(_):
     total_loss = mle_loss + kl_rate * kl_loss
 
     global_step = tf.placeholder(tf.int32)
-    train_op = tx.core.get_train_op(total_loss,
-                                    global_step=global_step,
-                                    increment_global_step=False,
-                                    hparams=opt_hparams)
+    # train_op = tx.core.get_train_op(total_loss,
+    #                                 global_step=global_step,
+    #                                 increment_global_step=False,
+    #                                 hparams=opt_hparams)
+    train_op = tx.core.get_train_op(total_loss, hparams=opt_hparams)
 
-    def _train_epochs(sess, epoch, display=10):
-        global new_kl_rate
+    def _train_epochs(sess, epoch, step, display=10):
 
         start_time = time.time()
         iterator.switch_to_train_data(sess)
@@ -233,7 +233,6 @@ def _main(_):
         t_nll = []
         t_kl_loss = []
         t_total_loss = []
-        step = 0
         while True:
             try:
                 fetches = {"train_op": train_op,
@@ -242,14 +241,12 @@ def _main(_):
                            "kl_loss": kl_loss,
                            "lengths": seq_lengths}
 
-                assert new_kl_rate <= 1.0
+                new_kl_rate = kl_anneal_function(step, anneal_hparams["k"],
+                                                 anneal_hparams["x0"])
                 feed = {tx.global_mode(): tf.estimator.ModeKeys.TRAIN,
                         kl_rate: new_kl_rate,
                         global_step: epoch}
                 fetches_ = sess.run(fetches, feed_dict=feed)
-                new_kl_rate = new_kl_rate + annealing_rate \
-                              if new_kl_rate <= 1.0 - annealing_rate \
-                              else 1.0
 
                 num_words += sum(fetches_["lengths"])
                 t_nll.append(fetches_["mle_loss"] + fetches_["kl_loss"])
@@ -272,6 +269,7 @@ def _main(_):
                       (epoch, np.mean(t_total_loss),
                        np.mean(t_nll), np.mean(t_kl_loss), log_ppl))
                 break
+        return step, new_kl_rate
 
     def _test_epochs(sess, epoch):
         iterator.switch_to_test_data(sess)
@@ -285,7 +283,7 @@ def _main(_):
                            "mle_loss": mle_loss,
                            "kl_loss": kl_loss,
                            "lengths": seq_lengths}
-                feed = {tx.global_mode(): tf.estimator.ModeKeys.TRAIN,
+                feed = {tx.global_mode(): tf.estimator.ModeKeys.EVAL,
                         kl_rate: 1.0,
                         global_step: epoch}
                 fetches_ = sess.run(fetches, feed_dict=feed)
@@ -308,9 +306,10 @@ def _main(_):
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
         sess.run(tf.tables_initializer())
+        step = 0
 
         for epoch in range(num_epochs):
-            _train_epochs(sess, epoch, display=200)
+            step, new_kl_rate = _train_epochs(sess, epoch, step, display=200)
             print('\nepoch %d, new kl rate %.4f\n' % (epoch, new_kl_rate))
             _test_epochs(sess, epoch)
 
