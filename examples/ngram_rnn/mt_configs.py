88a694d7a49d965298e4058b83cab5cc6dd97297
tan <tanbowen@sjtu.edu.cn>
2018-07-13 14:15:42 +0800

    example ngram_rnn added.
    Former-commit-id: 426fb9a39b659fcc74edb9acca6cc4b62f55b74c

---
+++ examples/ngram_rnn/mt_configs.py
@@ -0,0 +1,43 @@
+training_data_hparams = {
+    'num_epochs': 1,
+    'batch_size': 32,
+    'allow_smaller_final_batch': False,
+    'source_dataset': {
+        "files": ['data/iwslt14/train.de'],
+        'vocab_file': 'data/iwslt14/vocab.de',
+        'max_seq_length': 50
+    },
+    'target_dataset': {
+        'files': ['data/iwslt14/train.en'],
+        'vocab_file': 'data/iwslt14/vocab.en',
+        'max_seq_length': 50
+    }
+}
+
+valid_data_hparams = {
+    'num_epochs': 1,
+    'batch_size': 32,
+    'allow_smaller_final_batch': False,
+    'source_dataset': {
+        "files": ['data/iwslt14/valid.de'],
+        'vocab_file': 'data/iwslt14/vocab.de'
+    },
+    'target_dataset': {
+        'files': ['data/iwslt14/valid.en'],
+        'vocab_file': 'data/iwslt14/vocab.en'
+    }
+}
+
+test_data_hparams = {
+    'num_epochs': 1,
+    'batch_size': 32,
+    'allow_smaller_final_batch': False,
+    'source_dataset': {
+        "files": ['data/iwslt14/test.de'],
+        'vocab_file': 'data/iwslt14/vocab.de'
+    },
+    'target_dataset': {
+        'files': ['data/iwslt14/test.en'],
+        'vocab_file': 'data/iwslt14/vocab.en'
+    }
+}
