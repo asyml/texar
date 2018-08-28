9314d8ba88f064f6c49b73bf980bf7e5988aa14e
Haoran Shi <shore@pku.edu.cn>
2018-08-28 11:24:50 -0400

    updates
    Former-commit-id: 468a6c318adc03d035f359e948ab5e6d26dac6f0

--- examples/transformer/utils/preprocess.py
+++ examples/transformer/utils/preprocess.py
@@ -1,10 +1,22 @@
+# Copyright 2018 The Texar Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 preprocessing text data. Generally it's to generate plain text vocab file,
 truncate sequence by length, generate the preprocessed dataset.
 """
 from __future__ import unicode_literals
 import collections
-import io
 import re
 import json
 import os
@@ -220,16 +232,3 @@ if __name__ == "__main__":
         max_size = len(id2w)
         for idx in range(4, max_size):
             f.write('{}\n'.format(id2w[idx]))
-# Copyright 2018 The Texar Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
