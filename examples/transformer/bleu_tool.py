29da984672b479cf59a91b3072ec94cb35ada912
Haoran Shi <shore@pku.edu.cn>
2018-08-28 11:23:23 -0400

    updates
    Former-commit-id: 2aa76153758ff2d4cd61ca051a8cf51755b73497

--- examples/transformer/bleu_tool.py
+++ examples/transformer/bleu_tool.py
@@ -10,21 +10,6 @@
 # Note:compound splitting is not implemented in this module
 
 
-# coding=utf-8
-# Copyright 2018 The Tensor2Tensor Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """BLEU metric util used during eval for MT."""
 from __future__ import absolute_import
 from __future__ import division
