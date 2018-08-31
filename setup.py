50285e8315296a3a2d92dcc7354a902a217ef96d
Di Wang <diwang@cs.cmu.edu>
2018-08-31 14:27:10 -0400

    initial setup attempt for travis-ci

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-#
-"""
-Setup file.
-"""
-
 import setuptools
+
+
+long_description = '''
+Texar is an open-source toolkit based on Tensorflow, 
+aiming to support a broad set of machine learning especially text generation tasks, 
+such as machine translation, dialog, summarization, content manipulation, language modeling, and so on. 
+
+Texar is designed for both researchers and practitioners for fast prototyping and experimentation.
+'''
 
 setuptools.setup(
     name="texar",
@@ -11,6 +15,8 @@ setuptools.setup(
     url="https://github.com/asyml/texar",
 
     description="Toolkit for Text Generation and Beyond",
+    long_description=long_description,
+    license='Apache License Version 2.0',
 
     packages=setuptools.find_packages(),
     platforms='any',
@@ -21,11 +27,13 @@ setuptools.setup(
         'requests',
     ],
     extras_require={
-        'tensorflow': ['tensorflow>=1.7.0'],
-        'tensorflow with gpu': ['tensorflow-gpu>=1.7.0']
+        'tensorflow-cpu': ['tensorflow>=1.7.0'],
+        'tensorflow-gpu': ['tensorflow-gpu>=1.7.0']
     },
     classifiers=[
         'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.4',
