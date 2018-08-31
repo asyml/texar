96beaaeffcefe6e57334cc6ff52a043349c55d7e
Zhiting Hu <zhitinghu@gmail.com>
2018-08-31 15:30:52 -0400

    passed py2/3.5/3.6 pytest

--- texar/hyperparams_test.py
+++ texar/hyperparams_test.py
@@ -46,7 +46,7 @@ class HParamsTest(tf.test.TestCase):
         names = []
         for name, _ in hparams_.items():
             names.append(name)
-        self.assertEqual(names, list(default_hparams.keys()))
+        self.assertEqual(set(names), set(default_hparams.keys()))
 
         hparams = {
             "dict": {"key1": "new_value"},
