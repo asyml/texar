add test units for ScheduledStepsTrigger and fix some bugs

texar/utils/triggers_test.py
@@ -8,8 +8,9 @@ from __future__ import print_function
 
 import tensorflow as tf
 import random
+import bisect
 
-from texar.utils.triggers import Trigger, BestEverConvergenceTrigger
+from texar.utils.triggers import *
 
 
 class TriggerTest(tf.test.TestCase):
@@ -21,6 +22,62 @@ class TriggerTest(tf.test.TestCase):
         for step in range(100):
             trigger.trigger()
             self.assertEqual(trigger.user_state, step+1)
+
+
+class ScheduledStepsTriggerTest(tf.test.TestCase):
+    """Tests :class:`~texar.utils.ScheduledStepsTrigger`.
+    """
+
+    def test(self):
+        for i in range(100):
+            n = random.randint(1, 100)
+            m = random.randint(1, n)
+            p = random.uniform(0, 0.3)
+            f = lambda l, r: l // n != r // n
+            trigger = ScheduledStepsTrigger(0, lambda x: x+1, f)
+
+            last_called_step = None
+
+            for step in range(n):
+                if random.random() < p:
+                    if last_called_step is not None:
+                        triggered_ = f(last_called_step, step)
+                    else:
+                        triggered_ = False
+
+                    last_called_step = step
+
+                    triggered = trigger(step)
+
+                    self.assertEqual(trigger.last_called_step, last_called_step)
+                    self.assertEqual(triggered, triggered_)
+
+        for i in range(100):
+            n = random.randint(1, 100)
+            m = random.randint(1, n)
+            p = random.uniform(0, 0.3)
+            q = random.uniform(0, 0.3)
+            steps = [step for step in range(n) if random.random() < q]
+            f = lambda l, r: bisect.bisect_right(steps, l) < \
+                             bisect.bisect_right(steps, r)
+            trigger = ScheduledStepsTrigger(0, lambda x: x+1, steps)
+
+            last_called_step = -1
+
+            for step in range(n):
+                if random.random() < p:
+                    triggered_ = f(last_called_step, step)
+                    last_called_step = step
+
+                    triggered = trigger(step)
+
+                    self.assertEqual(triggered, triggered_)
+
+        trigger = ScheduledStepsTrigger(0, lambda x: x+1, [])
+        for step in range(100):
+            trigger.trigger()
+            self.assertEqual(trigger.user_state, step+1)
+
 
 class BestEverConvergenceTriggerTest(tf.test.TestCase):
     """Tests :class:`~texar.utils.BestEverConvergenceTrigger`.