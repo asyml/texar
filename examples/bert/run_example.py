ab0c8c81b3f1c4d7f4d50e9af97cee56afe8bd0d
Haoran Shi <haoranshi97@gmail.com>
2018-12-19 22:13:21 +0000

    rm

--- examples/bert/run_example.py
+++
@@ -1,7 +0,0 @@
- mpirun -np 2 \
-    -H 54.202.134.113:1,54.244.60.228:1\
-    -bind-to none -map-by slot \
-    -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
-    -mca pml ob1 -mca btl ^openib \
-    -mca btl_tcp_if_include ens3 \
-    python hvd_example.py
