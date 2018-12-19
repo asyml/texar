fa1eff47475b6f8800cb8555b3e4ba2f302ff014
Haoran Shi <haoranshi97@gmail.com>
2018-12-19 22:08:58 +0000

    updates

--- examples/bert/run.sh
+++ examples/bert/run.sh
@@ -1,6 +1,7 @@
  mpirun -np 2 \
-    -H localhost:1,shr@54.244.60.228:1\
+    -H 54.202.134.113:1,54.244.60.228:1\
     -bind-to none -map-by slot \
     -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
     -mca pml ob1 -mca btl ^openib \
+    -mca btl_tcp_if_include ens3 \
     python bert_classifier_main.py --do_train --do_eval --do_test --output_dir='multigpu_output'
