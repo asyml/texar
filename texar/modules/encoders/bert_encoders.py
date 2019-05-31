6fbb4ddda63a6d3f215dec7fbd8d326ef077e607
weiwei718 <weiwei2@andrew.cmu.edu>
2019-05-31 10:49:13 -0400

    polished bert code and docs

--- texar/modules/encoders/bert_encoders.py
+++ texar/modules/encoders/bert_encoders.py
@@ -105,13 +105,13 @@ class BertEncoder(BertBase, EncoderBase):
     def default_hparams():
         """Returns a dictionary of hyperparameters with default values.
 
-        * The encoder arch is determined by the constructor argument
-        :attr:`pretrained_model_name` if it's specified. In this case,
+        * The encoder arch is determined by the constructor argument \
+        :attr:`pretrained_model_name` if it's specified. In this case, \
         hparams are ignored.
-        * Otherwise, the encoder arch is determined by
-        `hparams['pretrained_model_name']` if it's specified. all other
+        * Otherwise, the encoder arch is determined by \
+        `hparams['pretrained_model_name']` if it's specified. all other \
         configs in hparams are ignored.`
-        * If the above two are `None`, the encoder arch is defined by
+        * If the above two are `None`, the encoder arch is defined by \
         the configs in hparams and weights are randomly initialized.
 
         .. code-block:: python
