557a3712611915d931401088be540fc84ae1aeea
Zhiting Hu <zhitinghu@gmail.com>
2019-07-28 18:59:55 -0400

    Update version to 0.2.1
    * Update version.py for 0.2.1

--- texar/version.py
+++ texar/version.py
@@ -14,7 +14,7 @@
 
 _MAJOR = "0"
 _MINOR = "2"
-_REVISION = "1-unreleased"
+_REVISION = "1"
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}".format(_MAJOR, _MINOR, _REVISION)
