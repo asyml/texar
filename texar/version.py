64da4aa79fb75d9a22df7ec48b4ce8ecd5805a17
Zhiting Hu <zhitinghu@gmail.com>
2019-08-05 14:27:52 -0400

    Update CHANGELOG.md and version.py for v0.2.2

--- texar/version.py
+++ texar/version.py
@@ -14,7 +14,7 @@
 
 _MAJOR = "0"
 _MINOR = "2"
-_REVISION = "2-unreleased"
+_REVISION = "2"
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}".format(_MAJOR, _MINOR, _REVISION)
