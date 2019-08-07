f168b96593fbef901e641024ba8545fdb5d3be11
AvinashBukkittu <avinashnb141@gmail.com>
2019-08-07 19:19:03 +0000

    Fix docs. Remove activation in 2nd layer of FF(bug fixed)

--- texar/modules/pretrained/xlnet_model_utils.py
+++ texar/modules/pretrained/xlnet_model_utils.py
@@ -44,29 +44,31 @@ class PositionWiseFF(ModuleBase):
         if activation == 'gelu':
             activation = layers.gelu
 
-        l1_hparams = {
-            "type": "Dense",
-            "kwargs": {
-                "units": ffn_inner_dim,
-                "activation": activation
+        with tf.variable_scope(self.variable_scope):
+            tf.get_variable_scope().set_initializer(
+                layers.get_initializer(self._hparams.initializer))
+            l1_hparams = {
+                "type": "Dense",
+                "kwargs": {
+                    "units": ffn_inner_dim,
+                    "activation": activation
+                }
             }
-        }
-        self.linear1 = layers.get_layer(hparams=l1_hparams)
-        dropout_hparams = {
-            "type": "Dropout",
-            "kwargs": {
-                "rate": dropout
+            self.linear1 = layers.get_layer(hparams=l1_hparams)
+            dropout_hparams = {
+                "type": "Dropout",
+                "kwargs": {
+                    "rate": dropout
+                }
             }
-        }
-        self.dropout = layers.get_layer(hparams=dropout_hparams)
-        l2_hparams = {
-            "type": "Dense",
-            "kwargs": {
-                "units": hidden_dim,
-                "activation": activation
+            self.dropout = layers.get_layer(hparams=dropout_hparams)
+            l2_hparams = {
+                "type": "Dense",
+                "kwargs": {
+                    "units": hidden_dim
+                }
             }
-        }
-        self.linear2 = layers.get_layer(hparams=l2_hparams)
+            self.linear2 = layers.get_layer(hparams=l2_hparams)
 
     @staticmethod
     def default_hparams():
@@ -75,8 +77,8 @@ class PositionWiseFF(ModuleBase):
         .. code-block:: python
 
             {
-                "hidden_dim": 1024,
-                "ffn_inner_dim": 4096,
+                "hidden_dim": 768,
+                "ffn_inner_dim": 3072,
                 "dropout": 0.1,
                 "activation": 'gelu'
             }
@@ -97,34 +99,55 @@ class PositionWiseFF(ModuleBase):
             See :func:`~texar.core.get_activation_fn` for more details.
         """
         return {
-            "name": "position_wise_FF",
-            "hidden_dim": 1024,
-            "ffn_inner_dim": 4096,
+            "name": "ff",
+            "initializer": None,
+            "hidden_dim": 768,
+            "ffn_inner_dim": 3072,
             "dropout": 0.1,
             "activation": 'gelu',
         }
 
-    def _build(self, input):
-        # Position-wise feed-forward
+    def _build(self, input, mode=None):
+        r"""Compute feed forward for the input.
+
+        Args:
+            input: Input tensor of size `(max_time, batch_size, hidden_dim)`
+            mode (optional): A tensor taking value in
+                :tf_main:`tf.estimator.ModeKeys <estimator/ModeKeys>`, including
+                `TRAIN`, `EVAL`, and `PREDICT`. If `None`, dropout is
+                controlled by :func:`texar.global_mode`.
+
+        :returns: A tensor output of the position wise feed forward network
+        """
+        is_training = is_train_mode(mode)
         output = self.linear1(input)
-        output = self.dropout(output)
+        output = self.dropout(output, training=is_training)
         output = self.linear2(output)
-        output = self.dropout(output)
+        output = self.dropout(output, training=is_training)
+
         # residual + layer norm
         output = tf.contrib.layers.layer_norm(
             input + output, begin_norm_axis=-1, scope=self.variable_scope,
             reuse=tf.AUTO_REUSE)
+
         return output
 
 
 class PositionalEmbedding(ModuleBase):
-    """Sinosoidal Positional Embedding."""
+    r"""Sinosoidal Positional Embedding."""
     def __init__(self, embed_dim):
         super().__init__()
         freq_seq = tf.range(0.0, embed_dim, 2.0)
         self.inv_freq = 1 / (10000 ** (freq_seq / embed_dim))
 
     def _build(self, pos_seq):
+        r"""Compute sinosoidal positional embeddings.
+
+        Args:
+            pos_seq: A 1D tensor of position sequences
+
+        :returns: A 2D tensor of sinosoidal embeddings for the sequence.
+        """
         pos_seq = tf.dtypes.cast(pos_seq, dtype=self.inv_freq.dtype)
         sinusoid_inp = tf.einsum('i,d->id', pos_seq, self.inv_freq)
         pos_emb = tf.concat([tf.sin(sinusoid_inp), tf.cos(sinusoid_inp)], -1)
@@ -132,16 +155,34 @@ class PositionalEmbedding(ModuleBase):
 
 
 class RelativePositionalEncoding(ModuleBase):
+    r"""Relative positional encodings."""
     def __init__(self, hparams=None):
         super().__init__(hparams)
         self.sinusoid_embed = PositionalEmbedding(self._hparams.dim)
 
     @staticmethod
     def default_hparams():
+        r"""Returns a dictionary of hyperparameters with default values.
+
+        .. code-block:: python
+
+            {
+                "dim": 768,
+                "max_seq_len": 512
+            }
+
+        Here
+
+        "dim": int
+            Dimension size of the positional embedding
+
+        "max_seq_len": int
+            Maximum size of the sequence length
+        """
         return {
             "name": "relative_positional_encoder",
-            "dim": 1024,
-            "max_seq_len": 512,
+            "dim": 768,
+            "max_seq_len": 512
         }
 
     def _create_positional_embedding(self, start, end, step, batch_size,
@@ -159,6 +200,34 @@ class RelativePositionalEncoding(ModuleBase):
 
     def _build(self, batch_size, seq_len, total_len, clamp_len=None,
                attn_type='bi', bi_data=True):
+        r"""Compute relative positional encoding.
+
+        Args
+            batch_size: int
+                Batch size of the input
+
+            seq_len: int
+                Sequence length of the input
+
+            total_len: int
+                Sequence length + Memory length
+
+            clamp_len (optional): int
+                Clamp all relative distances larger than clamp_len.
+                None means no clamping.
+
+            attn_type (optional): str
+                Attention type. Supported values are `"uni"` and `"bi"`.
+
+            bi_data (optional): bool
+                Whether to use bidirectional data input pipeline. Usually set to
+                True during pretraining and False during finetuning.
+
+        :returns: A tensor of shape `[total_len + seq_len, batch_size, dim]`
+            (if attn_type == `"bi"`) or of shape `[total_len, batch_size, dim]`
+            (if attn_type == `"uni"`) representing relative positional encoding
+            of the sequence.
+        """
         if attn_type == 'bi':
             start, end = total_len, -seq_len
         elif attn_type == 'uni':
@@ -181,7 +250,33 @@ class RelativePositionalEncoding(ModuleBase):
 
 
 class RelativeMutiheadAttention(ModuleBase):
-    def __init__(self, r_r_bias=None, r_w_bias=None, r_s_bias=None,
+    r"""Compute relative multi-head attention for XLNet encoder.
+
+    This module computes relative multi-head attention as explained in
+    `Transformer-XL, (Zihang et. al)` and in `XLNet (Zhiling et. al)`.
+
+    Args:
+        r_r_bias: A tensor of shape `(num_heads, head_dim)`.
+            The bias value added to query head while computing position based
+            attention score.
+
+        r_w_bias: A tensor of shape `(num_heads, head_dim)`.
+            The bias value added to query head while computing content based
+            attention score.
+
+        r_s_bias (optional): A tensor of shape `(num_heads, head_dim)`.
+            The bias value added to query head while computing segment based
+            attention score.
+
+        segment_embed (optional): A tensor of shape `(2, num_heads, head_dim)`
+            if use_segments is True. Otherwise, this is set to None.
+
+        hparams (dict or HParams, optional): Hyperparameters. Missing
+            hyperparameter will be set to default values. See
+            :meth:`default_hparams` for the hyperparameter sturcture
+            and default values.
+    """
+    def __init__(self, r_r_bias, r_w_bias, r_s_bias=None, segment_embed=None,
                  hparams=None):
         super().__init__(hparams=hparams)
 
@@ -194,19 +289,21 @@ class RelativeMutiheadAttention(ModuleBase):
                 tf.get_variable_scope().set_initializer(
                     layers.get_initializer(self._hparams.initializer))
 
-            self.head_projection = layers.get_layer(hparams={
-                "type": "Dense",
-                "kwargs": {"units": 3 * self.num_heads * self.head_dim,
-                           "use_bias": False}
-            })
+            # Official implementation creates these head variables.
+            # If we create dense layers instead, there would be dimension
+            # mismatch while loading the tensors
+            # TODO(avinash) : Can we reshape tensors while loading the ckpt?
+            self.q_head = tf.get_variable(
+                'q/kernel', [hidden_dim, self.num_heads, self.head_dim])
 
-            self.pos_projection = layers.get_layer(hparams={
-                "type": "Dense",
-                "kwargs": {
-                    "units": self.num_heads * self.head_dim,
-                    "use_bias": False
-                }
-            })
+            self.k_head = tf.get_variable(
+                'k/kernel', [hidden_dim, self.num_heads, self.head_dim])
+
+            self.v_head = tf.get_variable(
+                'v/kernel', [hidden_dim, self.num_heads, self.head_dim])
+
+            self.k_head_r = tf.get_variable(
+                'r/kernel', [hidden_dim, self.num_heads, self.head_dim])
 
             self.dropout = layers.get_layer(hparams={
                 "type": "Dropout",
@@ -222,36 +319,72 @@ class RelativeMutiheadAttention(ModuleBase):
                 }
             })
 
-            self.output_projection = layers.get_layer(hparams={
-                "type": "Dense",
-                "kwargs": {
-                    "units": hidden_dim,
-                    "use_bias": False
-                }
-            })
+            self.output_projection = tf.get_variable(
+                'o/kernel', [hidden_dim, self.num_heads, self.head_dim])
 
-            bias_shape = (self.num_heads, self.head_dim)
-            self.untie_r = r_r_bias is None
-            self.r_r_bias = (r_r_bias if r_r_bias is not None else
-                             tf.get_variable('r_r_bias', shape=bias_shape))
-            self.r_w_bias = (r_w_bias if r_w_bias is not None else
-                             tf.get_variable('r_w_bias', shape=bias_shape))
+            self.r_r_bias = r_r_bias
+            self.r_w_bias = r_w_bias
 
             if self._hparams.use_segments:
-                self.segment_embed = tf.get_variable(
-                    'segment_embedding', [2, self.num_heads, self.head_dim])
-                self.r_s_bias = (r_s_bias if r_s_bias is not None else
-                                 tf.get_variable('r_s_bias', shape=bias_shape))
+                self.segment_embed = segment_embed
+                self.r_s_bias = r_s_bias
 
             self.scale = 1 / (self.head_dim ** 0.5)
 
     @staticmethod
     def default_hparams():
+        r"""Returns a dictionary of hyperparameters with default values.
+
+        .. code-block:: python
+
+            {
+                "name": "rel_attn",
+                "initializer": None,
+                "num_heads": 12,
+                "hidden_dim": 768,
+                "head_dim": 64,
+                "dropout": 0.1,
+                "attention_dropout": 0.1,
+                "use_segments": True
+            }
+
+
+
+        Here:
+
+        The default parameters are values for cased XLNet-Base model.
+
+        "initializer" : dict, optional
+            Hyperparameters of the default initializer that initializes
+            variables created in this module.
+            See :func:`~texar.core.get_initializer` for details.
+
+        "num_heads": int
+            Number of heads in the attention
+
+        "hidden_dim": int
+            Hidden dimension of the embeddings
+
+        "head_dim": int
+            Size of the vectors after head projection.
+
+        "dropout": float
+            Dropout rate for layers
+
+        "attention_dropout": float
+            Dropout rate for attention layers
+
+        "use_segments": bool
+            Boolean to indicate if the input has segments
+
+        "name" : str
+            Name of the module.
+        """
         return {
-            "name": "relative_multihead_attention",
+            "name": "rel_attn",
             "initializer": None,
-            "num_heads": 16,
-            "hidden_dim": 1024,
+            "num_heads": 12,
+            "hidden_dim": 768,
             "head_dim": 64,
             "dropout": 0.1,
             "attention_dropout": 0.1,
@@ -303,29 +436,70 @@ class RelativeMutiheadAttention(ModuleBase):
 
         # attention probability
         attn_prob = tf.nn.softmax(attn_score, 1)
-        attn_prob = self.dropout(attn_prob,
-                                 training=is_training)
+        attn_prob = self.dropout_attn(attn_prob, training=is_training)
 
         # attention output
         attn_vec = tf.einsum('ijbn,jbnd->ibnd', attn_prob, v_head_h)
 
         return attn_vec
 
-    def _post_attention(self, attn_vec):
-        shape = tf.shape(attn_vec)
-        attn_vec = tf.reshape(
-            tensor=attn_vec,
-            shape=[shape[0], shape[1], self.num_heads*self.head_dim])
-        attn_out = self.output_projection(attn_vec)
-        attn_out = self.dropout(attn_out)
+    def _post_attention(self, attn_vec, mode=None):
+        is_training = is_train_mode(mode)
+        attn_out = tf.einsum('ibnd,hnd->ibh', attn_vec, self.output_projection)
+        attn_out = self.dropout(attn_out, training=is_training)
         return attn_out
 
     def _build(self, states_h, pos_embed, states_g=None, segment_mat=None,
                attn_mask_h=None, attn_mask_g=None, target_mapping=None,
                memory=None, mode=None):
-        shape = tf.shape(states_h)
-        seq_len, batch_size = shape[0], shape[1]
-        pos_len = tf.shape(pos_embed)[0]
+        r"""Compute relative multi-head attention with relative positional
+        encoding.
+
+        Args:
+            states_h: A content representation tensor of shape
+                `[seq_len, batch_size, hidden_dim]`
+
+            pos_embed: Position embedding tensor of shape
+                `[seq_len, batch_size, hidden_dim]`.
+
+            states_g (optional): A query representation tensor of shape
+                `[seq_len, batch_size, hidden_dim]`. This tensor is set during
+                decoding.
+
+            segment_mat (optional): A tensor of size
+                `[seq_len, tot_len, batch_size]` indicating if tokens are in the
+                 same seqment. A value at `(i, j, k)` of `1` indicates tokens at
+                  `i` and `j` are not in the same sequence in batch k.
+
+            attn_mask_h (optional): A tensor of shape
+                `[seq_len, seq_len, batch_size, 1]` Attention mask used while
+                computing attention score for `states_h`
+
+            attn_mask_g (optional): A tensor of shape
+                `[seq_len, seq_len, batch_size, 1]` Attention mask used while
+                computing attention score for `states_g`
+
+            target_mapping (optional): The target token mapping. Float tensor of
+                shape `[num_targets, seq_len, batch_size]`.
+                A value of 1 for ``target_mapping[i, j, k]`` indicates that
+                the `i`-th target token (in order of permutation) in batch `k`
+                is the token at position `j`.
+                Each row ``target_mapping[i, :, k]`` can have no more than one
+                value of 1.
+
+            memory (optional): Memory from previous batches. A list of length
+                `num_layers`, each a tensor of shape
+                `[mem_len, batch_size, hidden_dim]`.
+
+            mode (optional): A tensor taking value in
+                :tf_main:`tf.estimator.ModeKeys <estimator/ModeKeys>`, including
+                `TRAIN`, `EVAL`, and `PREDICT`. If `None`, dropout is
+                controlled by :func:`texar.global_mode`.
+
+        :returns: Returns output states for `states_h` and `states_g`
+            (`states_g` is not None)
+        """
+        batch_size = tf.shape(states_h)[1]
 
         if memory is not None and memory.shape.ndims > 1:
             concat_input = tf.concat([memory, states_h], axis=0)
@@ -333,27 +507,12 @@ class RelativeMutiheadAttention(ModuleBase):
             concat_input = states_h
 
         # Content heads.
-        heads = self.head_projection(concat_input)
-        q_head_h, k_head_h, v_head_h = tf.split(
-            heads, num_or_size_splits=3, axis=-1)
-        q_head_h = q_head_h[-seq_len:]
-        tot_len = tf.shape(k_head_h)[0]
-
-        q_head_h = tf.reshape(
-            tensor=q_head_h,
-            shape=[seq_len, batch_size, self.num_heads, self.head_dim])
-        k_head_h = tf.reshape(
-            tensor=k_head_h,
-            shape=[tot_len, batch_size, self.num_heads, self.head_dim])
-        v_head_h = tf.reshape(
-            tensor=v_head_h,
-            shape=[tot_len, batch_size, self.num_heads, self.head_dim])
+        q_head_h = tf.einsum('ibh,hnd->ibnd', states_h, self.q_head)
+        k_head_h = tf.einsum('ibh,hnd->ibnd', concat_input, self.k_head)
+        v_head_h = tf.einsum('ibh,hnd->ibnd', concat_input, self.v_head)
 
         # Positional heads.
-        k_head_r = self.pos_projection(pos_embed)
-        k_head_r = tf.reshape(
-            tensor=k_head_r,
-            shape=[pos_len, batch_size, self.num_heads, self.head_dim])
+        k_head_r = tf.einsum('ibh,hnd->ibnd', pos_embed, self.k_head_r)
 
         # Core attention ops.
         attn_vec_h = self._compute_attention_score(
@@ -361,15 +520,14 @@ class RelativeMutiheadAttention(ModuleBase):
             mode)
 
         # Post attention processing.
-        attn_out_h = self._post_attention(attn_vec_h)
+        attn_out_h = self._post_attention(attn_vec_h, mode=mode)
 
         output_h = tf.contrib.layers.layer_norm(
             attn_out_h + states_h, begin_norm_axis=-1,
             scope=self.variable_scope, reuse=tf.AUTO_REUSE)
 
         if states_g is not None:
-            heads_g = self.head_projection(states_g)
-            q_head_g, _, _ = tf.split(heads_g, num_or_size_splits=3, axis=-1)
+            q_head_g = tf.einsum('ibh,hnd->ibnd', states_g, self.q_head)
             shape = tf.shape(q_head_g)
             q_head_g = tf.reshape(
                 q_head_g,
@@ -383,7 +541,7 @@ class RelativeMutiheadAttention(ModuleBase):
             if target_mapping is not None:
                 attn_vec_g = tf.einsum(
                     'lbnd,mlb->mbnd', attn_vec_g, target_mapping)
-            attn_out_g = self._post_attention(attn_vec_g)
+            attn_out_g = self._post_attention(attn_vec_g, mode=mode)
             output_g = tf.contrib.layers.layer_norm(
                 attn_out_g + states_g, begin_norm_axis=-1,
                 scope=self.variable_scope, reuse=tf.AUTO_REUSE)
