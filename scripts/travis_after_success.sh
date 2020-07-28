24ae1034d86fd6a7606720c92555c7f9a2a8e213
hunterhector <hunterhector@gmail.com>
2020-07-27 23:39:04 -0400

    Add scripts for mirroring.

---
+++ scripts/travis_after_success.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+echo "This script is run automatically after_success."
+
+## The actions below tries to mirror the repo if this is run on the master branch of the main repoitory.
+mirror_from="https://github.com/asyml/texar.git"
+mirror_to="git@github.com:petuum/texar.git"
+
+if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_REPO_SLUG} == "asyml/texar" ]]; then
+  echo "Mirroring from "${mirror_from} " to "${mirror_to}
+
+  # Decrypt the private key.
+  openssl aes-256-cbc -k "$travis_key_password" -md sha256 -iter 100000 -d -a -in travis_key.enc -out ./travis_key
+  chmod 400 ./travis_key
+  echo "Host github.com" > ~/.ssh/config
+  echo "  IdentityFile $(pwd)/travis_key" >> ~/.ssh/config
+
+  git clone --bare ${mirror_from} forte_bare
+  if cd forte_bare; then
+    git push --mirror ${mirror_to}
+  else
+    echo "Cannot cd into forte_bare, clone may be unsuccessful."
+  fi
+else
+  echo "Not on the master of the main repo, will not do mirror."
+  echo "Travis Branch: ""${TRAVIS_BRANCH}"
+  echo "Travis PR: ""${TRAVIS_PULL_REQUEST}"
+  echo "Travis REPO: ""${TRAVIS_REPO_SLUG}"
+fi
+
+# More after_success commands can be added below.
